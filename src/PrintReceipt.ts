d8c415bc0154d7f4108cd2edd937988692b2a2ec
YChen110 <Ychen110@slb.com>
2023-10-31 21:40:54 +0800

    feat: generate receipt based on tag

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,8 +1,8 @@
-import { getItemByBarCode, loadAllItems } from './Dependencies';
+import { checkPromotionsExist, getItemByBarCode } from './Dependencies';
 import { ReceiptItem, Tag } from './Receipt.type';
 
 export function printReceipt(tags: string[]): string {
-	return `***<store earning no money>Receipt ***
+  return `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
 Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
@@ -13,62 +13,106 @@ Discounted prices：7.50(yuan)
 }
 
 
-function generateTag(rawItemList: string[]): Tag[] {
-	const tags: Tag[] = [];
-	return tags;
-
-}
-
-
-function generategReceiptItems(tags: Tag[]): ReceiptItem[] {
-	const receiptItems: ReceiptItem[] = [];
-	return receiptItems;
+export function generateTag(rawItemList: string[]) {
+  console.log(parseTag(rawItemList));
+  return parseTag(rawItemList);
 }
 
 function renderReceipt(receiptItems: ReceiptItem[]): string {
-	return '';
+  return '';
 }
 
-function parseTag(tags: string[]): Tag[] {
-	const allItems = loadAllItems();
-	const parsedTags: string[][] = tags.map(tag => parseQuantity(tag));
-	return parsedTags.map(tags => {
-		if (isValidBarcode(tags)) {
-			return {
-				item: tags[0],
-				quantity: tags[1],
-			} as Tag
-		}
-		else {
-			throw new Error("Invalid")
-		}
-	})
+export function parseTag(tags: string[]): Tag[] {
+  const parsedTags: string[][] = tags.map(tag => parseQuantity(tag));
+  return parsedTags.map(tags => {
+    if (isValidBarcode(tags)) {
+      return {
+        item: tags[0],
+        quantity: tags[1] ?? 1
+      } as Tag;
+    } else {
+      throw new Error('Invalid');
+    }
+  });
+}
+
+export function generateReceiptItems(tags: Tag[]): ReceiptItem[] {
+  const receiptItems: ReceiptItem[] = transferTagToReceiptItem(tags);
+  const itemsWithDiscount: ReceiptItem[] = calculateDiscount(receiptItems);
+  const finalReceiptItems: ReceiptItem[] = calculateItemSubtotal(itemsWithDiscount);
+  console.log(finalReceiptItems);
+  return receiptItems;
+}
+
+function transferTagToReceiptItem(tags: Tag[]): ReceiptItem[] {
+  const receiptItems: ReceiptItem[] = [];
+
+  tags.forEach(tag => {
+    const addedItem = receiptItems.find(item => item.barcode === tag.item);
+    if (addedItem) {
+      addedItem.quantity += Number(tag.quantity);
+    } else {
+      const item = getItemByBarCode(tag.item);
+      if (item) {
+        receiptItems.push({
+          name: item.name,
+          barcode: item.barcode,
+          quantity: Number(tag.quantity),
+          unitPrice: item.price,
+          unit: item.unit,
+          discount: 0,
+          subtotal: 0
+        });
+      }
+    }
+  });
+  return receiptItems;
+}
+
+function calculateDiscount(receiptItems: ReceiptItem[]): ReceiptItem[] {
+  return receiptItems.map(receiptItem => {
+    if (checkPromotionsExist(receiptItem.barcode)) {
+      receiptItem.discount = receiptItem.unitPrice * Math.floor(receiptItem.quantity / 3);
+    }
+    return receiptItem;
+  });
+}
+
+function calculateItemSubtotal(receiptItems: ReceiptItem[]): ReceiptItem[] {
+  return receiptItems.map(receiptItem => {
+    receiptItem.subtotal = receiptItem.quantity * receiptItem.unitPrice - receiptItem.discount;
+    return receiptItem;
+  });
 }
 
 function isValidBarcode(parsedTag: string[]): boolean {
-	return isValidItemCode(parsedTag[0]) && isValidQuantity(parsedTag);
+  return isValidItemCode(parsedTag[0]) && isValidQuantity(parsedTag);
 }
 
 function isValidQuantity(parsedTag: string[]): boolean {
-	const barcode = parsedTag[0];
-	const quantity = Number(parsedTag[1]);
-	if (getItemByBarCode(barcode)?.unit!= "pound") {
-		return Number.isNaN(quantity) && Number.isInteger(quantity);
-	}
-	return Number.isNaN(quantity);
+  const barcode = parsedTag[0];
+  if (parsedTag.length === 1) return true;
+  const quantity = Number(parsedTag[1]);
+  if (!Number.isNaN(quantity)) {
+    if (getItemByBarCode(barcode)?.unit !== 'pound') {
+      return Number.isInteger(quantity);
+    }
+    return true;
+  }
+  return false;
 
 }
 
 function isValidItemCode(itemCode: string): boolean {
-	if (getItemByBarCode(itemCode)){
-		return true;
-	}
-	return false;
+  if (getItemByBarCode(itemCode)) {
+    return true;
+  }
+  return false;
 }
 
 function parseQuantity(tag: string): string[] {
-	const tags = tag.split('-');
-	return tags;
+  const tags = tag.split('-');
+  return tags;
 }
 
 
